ada8a4b2a7274de00ed92f38980c4a1390ef215c
becker95-rgb <francobecker1995@gmail.com>
2026-05-26 18:05:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/logistica-rumipal/',
+    base: '/logisticarumipal/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
